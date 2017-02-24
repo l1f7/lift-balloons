d31cf4f12d24f55dad6fe2c7734071eb8fb8b729
Jeff <rustyangel@gmail.com>
2017-02-24 13:48:57 -0700

    🐛 Fix pip installation

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
     license='MIT',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    # install_requires=[],
-    zip_safe=False,
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',

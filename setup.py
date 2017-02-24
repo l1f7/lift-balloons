938bb4040272ad21a63acd62b1665d6f155d59ac
Jeff <rustyangel@gmail.com>
2017-02-24 11:14:22 -0700

    📦 Update PyPI description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='lift-balloons',
     version=__version__,
     description='A collection of helpful Django template tools for front-enders.',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     author='Lift Interactive',
     author_email='dev+pypi@liftinteractive.com',
     url='https://github.com/l1f7/lift-balloons',
